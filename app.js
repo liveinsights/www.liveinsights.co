305a35aed7f0e006d767808c3030b27fa1b82666
Silver Bullet <silverbullet@silverbullet.local>
2022-05-16 18:25:33 -0700

    updated package.json

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ if (app.get('env') === 'development') {
 
 // production error handler no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.ostatus || 500);
   res.render('error', {
     message: err.message,
     error: err
